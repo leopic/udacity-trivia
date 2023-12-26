131c7efe4fdf2cbe80ed09db1f98781e582496d4
Leo Picado <leonardo.picado@zenjob.com>
2023-12-26 14:36:39 +0100

    Add basic navigation

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -5,12 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.DataBindingUtil.inflate
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import com.example.android.navigation.databinding.FragmentTitleBinding
 
-/**
- * A simple [Fragment] subclass.
- * Use the [TitleFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class TitleFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,17 +24,13 @@ class TitleFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_title, container, false)
-    }
+        val binding: FragmentTitleBinding = inflate(inflater, R.layout.fragment_title, container, false)
 
-    companion object {
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TitleFragment().apply {
-                arguments = Bundle().apply {
-                }
-            }
+        binding.playButton.setOnClickListener(
+//            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+        )
+
+        return binding.root
     }
 }
