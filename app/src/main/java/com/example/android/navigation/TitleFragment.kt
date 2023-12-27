533e3df8c96b4904b52bd12bb91a04b0c72b7766
Leo Picado <leonardo.picado@zenjob.com>
2023-12-27 22:33:48 +0100

    Add menu

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -3,12 +3,16 @@ package com.example.android.navigation
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.DataBindingUtil.inflate
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
@@ -23,14 +27,26 @@ class TitleFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val binding: FragmentTitleBinding = inflate(inflater, R.layout.fragment_title, container, false)
+    ): View {
+        val binding: com.example.android.navigation.databinding.FragmentTitleBinding = inflate(inflater, R.layout.fragment_title, container, false)
 
         binding.playButton.setOnClickListener(
 //            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
             Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
         )
 
+        setHasOptionsMenu(true)
+
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, requireView().findNavController())
+                || super.onOptionsItemSelected(item)
     }
 }
