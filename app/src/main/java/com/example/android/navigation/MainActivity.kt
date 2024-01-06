ac7f2ee927d5a4d8b1d7b6bc5dec270cfba1fdc4
Leo Picado <leonardo.picado@zenjob.com>
2024-01-06 17:30:43 +0100

    Lock drawer when outside start destination

--- app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -20,6 +20,8 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.ActivityMainBinding
@@ -32,6 +34,12 @@ class MainActivity : AppCompatActivity() {
         drawerLayout = binding.drawerLayout
         val navController = this.findNavController(R.id.myNavHostFragment)
         NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        navController.addOnDestinationChangedListener() { nc: NavController, nd: NavDestination, args: Bundle? ->
+            val unlocked = DrawerLayout.LOCK_MODE_UNLOCKED
+            val locked = DrawerLayout.LOCK_MODE_LOCKED_CLOSED
+            val isStartDestination = nd.id == nc.graph.startDestination
+            drawerLayout.setDrawerLockMode(if (isStartDestination) unlocked else locked)
+        }
         NavigationUI.setupWithNavController(binding.navView, navController)
     }
 
